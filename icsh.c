4cdae768c4c78345afde5c44dcc2a1b0eb02bc1d
nonny898 <atichard.c@outlook.com>
2019-05-12 23:20:53 +0700

    12 May 23:20

--- icsh.c
+++ icsh.c
@@ -6,25 +6,44 @@
 #include <signal.h>
 #include <setjmp.h>
 
-#define BUFFERSIZE 1024
-#define TOKEN_BUFFERSIZE 64
-#define TOKEN_DELIM " \t\r\n\a"
+#define BUFFER_SIZE 2048
+#define TOKEN_BUFFER 64
+#define TOKEN_DELIMITERS " \t\r\n\a"
 
-pid_t pgid;
-pid_t pid;
-
-static jmp_buf env;
+static sigjmp_buf env;
 static volatile sig_atomic_t jump_active = 0;
+static int numActiveJobs = 0;
+int exit_status;
 
-void sigint_handler(int signo) {
+pid_t ppid;
+
+void sigint_handler() {
     if (!jump_active) {
         return;
     }
-    siglongjmp(env, 42);
+    siglongjmp(env, 115);
 }
+
+void signalHandler_child() {
+    pid_t pid;
+    int terminationStatus;
+    pid = waitpid(WAIT_ANY, &terminationStatus, WUNTRACED | WNOHANG);
+    if (pid > 0) {
+        if (WIFEXITED(terminationStatus)) {
+            exit_status = terminationStatus + 128;
+        } else if (WIFSIGNALED(terminationStatus)) {
+            exit_status = terminationStatus + 128;
+        } else if (WIFSTOPPED(terminationStatus)) {
+            exit_status = terminationStatus + 128;
+        } else {
+            exit_status = terminationStatus + 128;
+        }
+    }
+}
+
 char *input(void) {
 
-    int bufferSize = BUFFERSIZE;
+    int bufferSize = BUFFER_SIZE;
     int position = 0;
     char *buffer = malloc(sizeof(char) * bufferSize);
     int c;
@@ -46,47 +65,73 @@ char *input(void) {
 }
 
 char **arguments(char *line) {
-    int bufferSize = TOKEN_BUFFERSIZE, position = 0;
+    int bufferSize = TOKEN_BUFFER, position = 0;
     char **tokens = malloc(bufferSize * sizeof(char *));
     char *token;
 
     if (line[0] == '\0') {
         tokens[position] = "\0";
     } else {
-        token = strtok(line, TOKEN_DELIM);
+        token = strtok(line, TOKEN_DELIMITERS);
         while (token != NULL) {
             tokens[position] = token;
             position++;
-            token = strtok(NULL, TOKEN_DELIM);
+            token = strtok(NULL, TOKEN_DELIMITERS);
         }
         tokens[position] = NULL;
-        return tokens;
     }
+    return tokens;
 }
 
+/*
+ *  Print out correct exit status with “echo $?”
+ *  “Exit” command works.
+ *
+ */
 int builtInCommands(char **args) {
-    if (strcmp("exit", args[0]) == 0) {
+
+    if ((strcmp("Exit", args[0]) == 0) || (strcmp("exit", args[0]) == 0)) {
         exit(EXIT_SUCCESS);
+    }
+    if ((strcmp("echo",args[0]) == 0) && (strcmp("$?", args[1]) == 0)) {
+        printf("%d\n",exit_status);
+        exit_status = 0;
         return 1;
     }
     else {
-//        printf("%s",args[0]);
+        return 0;
     }
-    return 0;
 }
 
+/*
+ * Correctly execute a foreground program.
+ * The program starts, runs to completion and return to the prompt.
+ * Support executing a program with arguments
+ */
 void launcher(char **args) {
-    pid = fork();
-    if (pid == 0) {
-        signal(SIGINT,SIG_DFL);
-//        execvp(args[0],args);
-        for (int i = 0; i < 1000000000; ++i) {
 
-        }
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+        printf("Can't create child process\n");
+        exit(EXIT_FAILURE);
     }
-    else {
+    else if (pid == 0) {
+
+        signal(SIGINT, SIG_DFL);
+        signal(SIGQUIT, SIG_DFL);
+        signal(SIGTSTP, SIG_DFL);
+        signal(SIGCHLD, &signalHandler_child);
+        signal(SIGTTIN, SIG_DFL);
+
+        usleep(2000000);
+        execvp(args[0], args);
+        exit(EXIT_SUCCESS);
+
+
+    } else {
         wait(NULL);
-        printf("%d\n",pid);
     }
 }
 
@@ -96,55 +141,46 @@ void execute(char **args) {
     }
 }
 
-
 #pragma clang diagnostic push
-
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+
+/*
+ * Compiled properly without any warning or error.
+ * A prompt, “icsh>” , is display correctly after being run.
+ * Empty command does nothing.
+ * Ctrl+C does not exit the shell.
+ */
 void shellPrompt() {
     char *line;
     char **args;
     int status;
 
-    pgid = getpid();
+    ppid = getpid();
 
-//        struct sigaction as;
-    //    signal(SIGINT, SIG_IGN);
-//        as.sa_flags = SA_RESTART;
-//        sigaction(SIGINT,&as,NULL);
-
-    struct sigaction s;
-    s.sa_handler = sigint_handler;
-    sigemptyset(&s.sa_mask);
-    s.sa_flags = SA_RESTART;
-    sigaction(SIGINT, &s, NULL);
+    signal(SIGINT, sigint_handler);
+    signal(SIGCHLD, &signalHandler_child);
 
     do {
 
-        if (sigsetjmp(env, 1) == 42) {
+        if (sigsetjmp(env, 1) == 115) {
             printf("\n");
         }
         jump_active = 1;
-        printf("icsh> ");
+        printf("icsh > ");
         line = input();
         args = arguments(line);
         execute(args);
-
-//        status = lsh_execute(args);
 
         free(line);
         free(args);
 
     } while (1);
 }
+
 #pragma clang diagnostic pop
 
-void signalHandler(){
-    shellPrompt();
-}
-
 int main() {
-    printf("Hello");
-    signal(SIGINT, signalHandler);
     shellPrompt();
     return EXIT_SUCCESS;
 }
